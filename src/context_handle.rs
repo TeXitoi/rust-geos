2de9de1ab95cc1c9e6d2d9df23ffd35e1d1f95ba
Guillaume P <texitoi@texitoi.eu>
2019-04-01 15:39:21 +0200

    Draft proposition for context_handler

--- src/context_handle.rs
+++ src/context_handle.rs
@@ -3,18 +3,20 @@ use c_vec::CVec;
 use enums::{ByteOrder, Dimensions};
 use error::{Error, GResult};
 use ffi::*;
-use functions::*;
-use libc::{c_char, c_void, strlen};
-use std::cell::RefCell;
-use std::ffi::CStr;
-use std::slice;
+use std::sync::{Arc, Mutex};
 
 pub struct GContextHandle {
     ptr: GEOSContextHandle_t,
-    // TODO: maybe store the closure directly?
-    notice_message: RefCell<*mut c_void>,
-    // TODO: maybe store the closure directly?
-    error_message: RefCell<*mut c_void>,
+    // boxed for stable address
+    messages: Arc<Mutex<Messages>>,
+    notice_closure: Box<dyn Fn(&str)>,
+    error_closure: Box<dyn Fn(&str)>,
+}
+
+#[derive(Default)]
+struct Messages {
+    last_error: Option<String>,
+    last_notice: Option<String>,
 }
 
 impl GContextHandle {
@@ -25,110 +27,38 @@ impl GContextHandle {
     /// ```
     /// use geos::GContextHandle;
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     /// ```
-    pub fn init() -> GResult<Self> {
-        initialize();
+    pub fn new() -> GResult<Self> {
         let ptr = unsafe { GEOS_init_r() };
         if ptr.is_null() {
-            Err(Error::GenericError("GEOS_init_r failed".to_owned()))
-        } else {
-            Ok(GContextHandle {
-                ptr,
-                notice_message: RefCell::new(::std::ptr::null_mut()),
-                error_message: RefCell::new(::std::ptr::null_mut()),
-            })
+            return Err(Error::GenericError("GEOS_init_r failed".to_owned()))
         }
+        let messages = Arc::new(Mutex::new(Messages::default()));
+        let notice_closure = {
+            let messages = messages.clone();
+            move |s: &str| messages.lock().unwrap().last_notice = Some(s.to_string())
+        };
+        let error_closure = {
+            let messages = messages.clone();
+            move |s: &str| messages.lock().unwrap().last_error = Some(s.to_string())
+        };
+        let res = GContextHandle {
+            ptr,
+            messages,
+            notice_closure: Box::new(notice_closure),
+            error_closure: Box::new(error_closure),
+        };
+        // TODO: set handlers...
+        Ok(res)
     }
 
-    /// Allows to set a notice message handler.
-    ///
-    /// Passing [`None`] as parameter will unset this callback.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use geos::GContextHandle;
-    ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
-    ///
-    /// context_handle.set_notice_message_handler(Some(Box::new(|s| println!("new message: {}", s))));
-    /// ```
-    pub fn set_notice_message_handler<'a>(&'a self, nf: Option<Box<dyn Fn(&str) + 'a>>) {
-        let nf_data: Box<Option<Box<dyn Fn(&str) + 'a>>> = Box::new(nf);
-
-        unsafe extern "C" fn message_handler_func<'a>(message: *const c_char, data: *mut c_void) {
-            let callback: &Option<Box<dyn Fn(&str) + 'a>> = &*(data as *mut _);
-
-            let bytes = slice::from_raw_parts(message as *const u8, strlen(message));
-            if let Some(ref callback) = *callback {
-                let s = CStr::from_bytes_with_nul_unchecked(bytes);
-                callback(s.to_str().expect("invalid CStr -> &str conversion"))
-            } else {
-                panic!("cannot get closure...")
-            }
-        }
-
-        let page_func = if nf_data.is_some() {
-            Some(message_handler_func as _)
-        } else {
-            None
-        };
-        let nf_data = Box::into_raw(nf_data) as *mut _;
-        let previous_ptr = self.notice_message.replace(nf_data);
-        if !previous_ptr.is_null() {
-            // We free the previous closure.
-            let _callback: Box<Option<Box<dyn Fn(&str) + 'a>>> =
-                unsafe { Box::from_raw(previous_ptr as *mut _) };
-        }
-        unsafe {
-            GEOSContext_setNoticeMessageHandler_r(self.ptr, page_func, nf_data);
-        }
+    pub fn take_last_notice(&self) -> Option<String> {
+        self.messages.lock().unwrap().last_notice.take()
     }
 
-    /// Allows to set an error message handler.
-    ///
-    /// Passing [`None`] as parameter will unset this callback.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use geos::GContextHandle;
-    ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
-    ///
-    /// context_handle.set_error_message_handler(Some(Box::new(|s| println!("new message: {}", s))));
-    /// ```
-    pub fn set_error_message_handler<'a>(&'a self, ef: Option<Box<dyn Fn(&str) + 'a>>) {
-        let ef_data: Box<Option<Box<dyn Fn(&str) + 'a>>> = Box::new(ef);
-
-        unsafe extern "C" fn message_handler_func<'a>(message: *const c_char, data: *mut c_void) {
-            let callback: &Option<Box<dyn Fn(&str) + 'a>> = &*(data as *mut _);
-
-            let bytes = slice::from_raw_parts(message as *const u8, strlen(message));
-            if let Some(ref callback) = *callback {
-                let s = CStr::from_bytes_with_nul_unchecked(bytes);
-                callback(s.to_str().expect("invalid CStr -> &str conversion"))
-            } else {
-                panic!("cannot get closure...")
-            }
-        }
-
-        let page_func = if ef_data.is_some() {
-            Some(message_handler_func as _)
-        } else {
-            None
-        };
-        let ef_data = Box::into_raw(ef_data) as *mut _;
-        let previous_ptr = self.notice_message.replace(ef_data);
-        if !previous_ptr.is_null() {
-            // We free the previous closure.
-            let _callback: Box<Option<Box<dyn Fn(&str) + 'a>>> =
-                unsafe { Box::from_raw(previous_ptr as *mut _) };
-        }
-        unsafe {
-            GEOSContext_setErrorMessageHandler_r(self.ptr, page_func, ef_data);
-        }
+    fn get_error(&self) -> String {
+        self.messages.lock().unwrap().last_error.take().unwrap_or("unknown error".to_string())
     }
 
     /// Gets WKB output dimensions.
@@ -138,7 +68,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, Dimensions};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     ///
     /// context_handle.set_wkb_output_dimensions(Dimensions::TwoD);
     /// assert!(context_handle.get_wkb_output_dimensions() == Dimensions::TwoD);
@@ -154,7 +84,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, Dimensions};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     ///
     /// context_handle.set_wkb_output_dimensions(Dimensions::TwoD);
     /// assert!(context_handle.get_wkb_output_dimensions() == Dimensions::TwoD);
@@ -170,7 +100,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, ByteOrder};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     ///
     /// context_handle.set_wkb_byte_order(ByteOrder::LittleEndian);
     /// assert!(context_handle.get_wkb_byte_order() == ByteOrder::LittleEndian);
@@ -186,7 +116,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, ByteOrder};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     ///
     /// context_handle.set_wkb_byte_order(ByteOrder::LittleEndian);
     /// assert!(context_handle.get_wkb_byte_order() == ByteOrder::LittleEndian);
@@ -202,7 +132,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, GGeom};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     /// let point_geom = GGeom::new("POINT (2.5 2.5)").expect("Invalid geometry");
     /// let wkb_buf = context_handle.geom_to_wkb_buf(&point_geom)
     ///                             .expect("conversion to WKB failed");
@@ -221,7 +151,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, GGeom};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     /// let point_geom = GGeom::new("POINT (2.5 2.5)").expect("Invalid geometry");
     /// let wkb_buf = context_handle.geom_to_wkb_buf(&point_geom)
     ///                             .expect("conversion to WKB failed");
@@ -229,14 +159,14 @@ impl GContextHandle {
     ///                              .expect("conversion from WKB failed");
     /// assert!(point_geom.equals(&new_geom) == Ok(true));
     /// ```
-    pub fn geom_to_wkb_buf(&self, g: &GGeom) -> Option<CVec<u8>> {
+    pub fn geom_to_wkb_buf(&self, g: &GGeom) -> GResult<CVec<u8>> {
         let mut size = 0;
         unsafe {
             let ptr = GEOSGeomToWKB_buf_r(self.ptr, g.as_raw(), &mut size);
             if ptr.is_null() {
-                None
+                Err(Error::GenericError(self.get_error()))
             } else {
-                Some(CVec::new(ptr, size as _))
+                Ok(CVec::new(ptr, size as _))
             }
         }
     }
@@ -248,7 +178,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, GGeom};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     /// let point_geom = GGeom::new("POINT (2.5 2.5)").expect("Invalid geometry");
     /// let wkb_buf = context_handle.geom_to_hex_buf(&point_geom)
     ///                             .expect("conversion to HEX failed");
@@ -267,7 +197,7 @@ impl GContextHandle {
     /// ```
     /// use geos::{GContextHandle, GGeom};
     ///
-    /// let context_handle = GContextHandle::init().expect("invalid init");
+    /// let context_handle = GContextHandle::new().expect("invalid init");
     /// let point_geom = GGeom::new("POINT (2.5 2.5)").expect("Invalid geometry");
     /// let wkb_buf = context_handle.geom_to_hex_buf(&point_geom)
     ///                             .expect("conversion to HEX failed");
@@ -292,18 +222,6 @@ impl Drop for GContextHandle {
     fn drop<'a>(&'a mut self) {
         unsafe { GEOS_finish_r(self.ptr) };
 
-        let previous_ptr = self.notice_message.replace(::std::ptr::null_mut());
-        if !previous_ptr.is_null() {
-            // We free the previous closure.
-            let _callback: Box<Option<Box<dyn Fn(&str) + 'a>>> =
-                unsafe { Box::from_raw(previous_ptr as *mut _) };
-        }
-
-        let previous_ptr = self.error_message.replace(::std::ptr::null_mut());
-        if !previous_ptr.is_null() {
-            // We free the previous closure.
-            let _callback: Box<Option<Box<dyn Fn(&str) + 'a>>> =
-                unsafe { Box::from_raw(previous_ptr as *mut _) };
-        }
+        // TODO: cleanup handlers
     }
 }
